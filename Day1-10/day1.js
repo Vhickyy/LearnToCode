d79c5ccbd7775435866abb8224be32c42f832c49
Victoria Okonnah <victoriaokonnah@gmail.com>
2023-12-25 12:48:26 -0800

    day one challenge

---
+++ Day1-10/day1.js
@@ -0,0 +1,26 @@
+
+// Variable Declaration: var, let and const
+var name = "Victoria";
+let favFood = "Rice";
+const randomNumber = 3; 
+
+
+// Data Types
+// Primitive Data Types: String, Number, Boolean, undefined, null, symbol
+// Reference Data Types: Object
+
+// String
+const userName = "Vhic_kee";
+// Number
+const number = 20;
+// Boolean
+const boolean = true;
+// null
+const hobby = null;
+
+// Comments: They are ignored during code execution and are useful for giving more information about the code.
+// Single line comment
+/*
+Multiline comment 
+*/
+
