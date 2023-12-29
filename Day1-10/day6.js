af9c200249d1851eeca75a2b0d8e487e08adc114
Victoria Okonnah <victoriaokonnah@gmail.com>
2023-12-29 20:48:11 +0100

    loops on day 6

--- Day1-10/day6.js
+++ Day1-10/day6.js
@@ -1,2 +1,28 @@
 // loops
 // for loops, while loops, do while loops
+
+const myName = "Victoria"
+
+for(let i = 0; i < myName.length; i++){
+    if(i == 3){
+        continue;
+    }
+    console.log(myName[i]);
+}
+
+let count = 0;
+while(count < myName.length){
+    if(count == 3){
+        break;
+    }
+    console.log(myName[count]);
+    count++;
+}
+
+let num = 0;
+// will run at least once
+do{
+    num++;
+}while(num < 1){
+    console.log(num);
+}
