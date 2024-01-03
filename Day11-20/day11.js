bb0e14b68761f295c97469fb93f7daeba12c911e
Victoria Okonnah <victoriaokonnah@gmail.com>
2024-01-03 20:05:15 +0100

    completed day11

---
+++ Day11-20/day11.js
@@ -0,0 +1,20 @@
+// functions
+// function declaration
+
+function greet(){
+    console.log("Good day!!");
+}
+greet()
+
+// function expression
+const favNum = function(){
+    console.log(3);
+}
+favNum()
+
+//Arrow function
+const sayMyName = (name) => {
+    console.log(name);
+}
+
+sayMyName("Victoria")
