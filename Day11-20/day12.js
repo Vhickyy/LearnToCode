c98918ffb5583f44ec116968a0c5e7828301ea68
Victoria Okonnah <victoriaokonnah@gmail.com>
2024-01-04 22:41:24 +0100

    day 12 done

---
+++ Day11-20/day12.js
@@ -0,0 +1,28 @@
+// objects
+const girl = {
+    name: "Victoria",
+    height: "6ft",
+    favFood: {
+        breakfast: "Rice",
+        lunch: "Noodles",
+        dinner: ["Rice", "cake"]
+    },
+    isNice: true,
+    favNumbers: {
+        odd: 1,
+        even: 2
+    }
+}
+console.log(girl.name);
+// Victoria
+console.log(girl.height);
+// 6ft
+const oddNumber = "odd"
+console.log(girl.favFood[oddNumber]);
+// 1
+const dinner = girl.favFood.dinner
+console.log(dinner);
+// ["Rice", "cake"]
+delete girl.isNice
+console.log(girl.isNice);
+// undefined
