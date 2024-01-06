da671e223f486d09aefd4b1333b1b67024be0ede
Victoria Okonnah <victoriaokonnah@gmail.com>
2024-01-06 22:47:24 +0100

    day 14 done

--- Day11-20/day14.js
+++ Day11-20/day14.js
@@ -1,1 +1,22 @@
 // Destructuring
+const countries = ["Ghana", "Kenya", "Nigeria", "Italy", "Canada"];
+
+const [firstCountry, secondCountry, ,fouthCounytry] = countries;
+console.log(firstCountry);
+// Nigeria
+console.log(secondCountry);
+// Kenya
+console.log(fouthCounytry);
+// Italy
+
+const numbers = [];
+const obj = { a: 1, b: 2 };
+const {a,b} = obj;
+console.log(a);
+// 1
+console.log(b);
+// 2
+
+({ a: numbers[0], b: numbers[1] } = obj);
+console.log(numbers);
+// [1,2]
