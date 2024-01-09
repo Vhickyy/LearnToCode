a6921fa9aaf34c2c4958d1e5c3b7d642f3528abd
Victoria Okonnah <victoriaokonnah@gmail.com>
2024-01-09 20:55:41 +0100

    day17 done

---
+++ Day11-20/day17.js
@@ -0,0 +1,17 @@
+// Type coercion
+
+const boolSum = true + false;
+console.log(boolSum);
+// 1
+
+const sum = "1" + 7;
+console.log(sum);
+// 17
+
+const arr = [];
+console.log([] + 7);
+// 7
+
+const multiply = "2" * "5";
+console.log(multiply);
+// 10
