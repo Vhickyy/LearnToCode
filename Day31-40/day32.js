160793cae4cf2be172e7cb52973dbd3b8c427300
Victoria Okonnah <victoriaokonnah@gmail.com>
2024-01-25 00:08:07 +0100

    day 32 done

---
+++ Day31-40/day32.js
@@ -0,0 +1,17 @@
+// for in loop
+
+const favColorObj = {
+    Victoria: "Blue",
+    Peace : "White",
+    Rose: "Red",
+    Jane: "Pink"
+}
+
+for (student in favColorObj){
+    console.log(`${student}'s favorite color is ${favColorObj[student]}`);
+}
+
+// Victoria's favorite color is Blue
+// Peace's favorite color is White
+// Rose's favorite color is Red
+// Jane's favorite color is Pink
