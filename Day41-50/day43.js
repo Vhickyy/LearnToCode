d85f4f773e9b6acdabcd8d6ded711141a133b967
Victoria Okonnah <victoriaokonnah@gmail.com>
2024-02-04 21:29:09 +0100

    day 43 done

--- Day41-50/day43.js
+++ Day41-50/day43.js
@@ -8,17 +8,18 @@ console.log(heading);
 // <h1 id="h1">Learning Javascript</h1>
 
 console.log(paragraph);
-// HTMLCollection(1)
+// HTMLCollection [p.paragraph]
 
 console.log(firstHeading);
 // <h1 id="h1">Learning Javascript</h1>
 
 console.log(headings);
-// NodeList(2)
+// NodeList(2) [h1#h1, h1]
+
 const headingArr = [...headings];
 headingArr.map(heading => console.log(heading));
 // <h1 id="h1">Learning Javascript</h1>
 // <h1>second heading</h1>
 
 console.log(buttons);
-// HTMLCollection(2)
+// HTMLCollection(2) [button, button]
